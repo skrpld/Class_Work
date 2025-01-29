fb87f9c22004cb00d09cec76bc084c0b4b15e4ca
skrpld <lesenkokirill637@gmail.com>
2025-01-29 12:22:33 +0300

    start
    - create java project
    - make Practic_2

---
+++ Practics/Practic_2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введите x:");
+        double x = scan.nextDouble();
+        System.out.println("Введите n:");
+        double n = scan.nextDouble();
+        double res = 0 - Math.PI / 2;
+
+        for (double i = 0; i <= n; i++) {
+            res += Math.pow(-1, i + 1) / ((2 * i + 1) * Math.pow(x, 2 * i + 1));
+        }
+
+        System.out.printf("Результат функции = %f", res);
+    }
+}
