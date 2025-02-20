eb82db2b322f0c69f155586a21829eab10e624bf
skrpld <lesenkokirill637@gmail.com>
2025-02-20 22:27:13 +0300

    In progress

--- Practic_4.java
+++ Practic_4.java
@@ -1,38 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        private static class Node {
-            int data;
-            Node left;
-            Node right;
-            Node(int data) {
-                this.data = data;
-            }
+        Tree tree = new Tree();
+        tree.add(1);
+    }
+}
+class Tree {
+    private static class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data) {
+            this.data = data;
         }
-        public static class Tree {
-            private Node root;
+    }
+    private Node root;
 
-            public void add(int data) {
-                root = recAdd(root, data);
-            }
+    public void add(int data) {
+        root = recAdd(root, data);
+    }
 
-            private Node recAdd(Node current, int data) {
-                if (current == null) {
-                    return new Node(data);
-                }
-                if (data < current.data) {
-                    current.left = recAdd(current.left, data);
-                }
-                else if (data > current.data) {
-                    current.right = recAdd(current.right, data);
-                }
-                else {
-                    return current;
-                }
-
-                return current;
-            }
+    private Node recAdd(Node current, int data) {
+        if (current == null) {
+            return new Node(data);
+        }
+        if (data < current.data) {
+            current.left = recAdd(current.left, data);
+        }
+        else if (data > current.data) {
+            current.right = recAdd(current.right, data);
+        }
+        else {
+            return current;
         }
 
-//        Tree.add(1);
+        return current;
     }
 }
