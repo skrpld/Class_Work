80aaa3b2201f3b1ebb3f734e9609641cad5bc40d
skrpld <lesenkokirill637@gmail.com>
2025-03-01 17:26:23 +0300

    Update Practic_4.java

--- Practic_4.java
+++ Practic_4.java
@@ -1,7 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         Tree tree = new Tree();
-        tree.add(1);
+
+        int[][] array = {
+                {3, 7, 1, 9, 44}};
+        String[] resultString = new String[5]; //длина не меньше длины самого длинного вложеного массива
+
+        for (int[] j : array) {
+            for (int i : j) {
+                tree.add(i);
+            }
+            resultString = tree.output().split(" ");
+            for (int i = 0; i < j.length; i++) {
+                j[i] = Integer.parseInt(resultString[i]);
+            }
+        }
+
+        for (int[] j : array) {
+            System.out.print("| ");
+            for (int i : j) {
+                System.out.print(i + " | ");
+            }
+        }
+
+        tree.output();
     }
 }
 class Tree {
@@ -32,7 +54,21 @@ class Tree {
         else {
             return current;
         }
-
         return current;
+    }
+
+    String result = null;
+    public String output() {
+        result = output(root, result);
+        return result;
+    }
+    private String output(Node node, String result) {
+        if (node != null) {
+            output(node.left, result);
+            result += (node.data + " ");
+            output(node.right, result);
+
+        }
+        return result;
     }
 }
