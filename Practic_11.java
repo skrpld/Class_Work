f4adee3f762c5295883733f0cdc9cb3b842b7eeb
skrpld <lesenkokirill637@gmail.com>
2025-03-14 10:09:06 +0300

    Update Practic_11.java

--- Practic_11.java
+++ Practic_11.java
@@ -5,27 +5,39 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         Student[] people = {
-                new Student( 30, "Иван", "123", "второй", "компьютеры", 2000),
-                new Student( 30, "Антон", "456", "третий", "коипьютеры", 2007),
-                new Student( 30, "Серёга", "789", "первый", "языки", 20008),
+                new Student(30, "Иван", "123", "второй", "компьютеры", 2000),
+                new Student(30, "Антон", "456", "третий", "компьютеры", 2007),
+                new Student(30, "Серёга", "789", "первый", "языки", 2008),
         };
 
         for (Student student : people) {
             System.out.printf("Ф.И.О.: %s | Возраст: %s\n", student.name, student.getAge());
         }
 
+        System.out.print("Введите факультет: ");
         String faculty = scan.next();
+        boolean facultyFound = false;
         for (Student student : people) {
-            if (faculty == student.faculty) {
+            if (faculty.equals(student.faculty)) {
                 System.out.printf("Ф.И.О.: %s | Возраст: %s\n", student.name, student.getAge());
+                facultyFound = true;
             }
         }
+        if (!facultyFound) {
+            System.out.println("Студентов на факультете '" + faculty + "' не найдено.");
+        }
 
+        System.out.print("Введите год: ");
         int enterYear = scan.nextInt();
+        boolean yearFound = false;
         for (Student student : people) {
-            if (enterYear > student.getEnterYear()) {
+            if (student.getEnterYear() > enterYear) {
                 System.out.printf("Ф.И.О.: %s | Возраст: %s\n", student.name, student.getAge());
+                yearFound = true;
             }
+        }
+        if (!yearFound) {
+            System.out.println("Нет студентов, поступивших после " + enterYear + " года.");
         }
     }
 }
@@ -40,7 +52,7 @@ class Person {
         this.name = name;
         this.phone = phone;
     }
-    
+
     int getAge() {
         return age;
     }
